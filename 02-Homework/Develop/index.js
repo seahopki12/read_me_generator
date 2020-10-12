37ac7da1ebdf36478dc2d210929ef98cf1fe8a26
Sean Hopkins <seahopki12@gmail.com>
2020-10-12 18:08:05 -0400

    relocated questions

--- 02-Homework/Develop/index.js
+++ 02-Homework/Develop/index.js
@@ -2,11 +2,60 @@ var fs = require("fs");
 var inquirer = require("inquirer");
 
 // array of questions for user
-// const questions = [
-//     "What is the title of your project? ", "Please write a description for your project: ", "Please include any installation instructions for your project: ", "Please include any usage information for your project: ", "Please include any contribution guidelines for your project: ", "Please include any test instructions for your project: ", "Please select any necessary licenses from the list below for your application: ", "Please enter your GitHub username: ", "Please enter your email address: "
-// ];
-// const names = ["title", "description", "installation", "usage", "contribution", "test", "licenses", "github", "email"];
-// const types = ["input", "input", "input", "input", "input", "input", "input", "checkbox", "input", "input"];
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "What is the title of your project? "
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Please write a description for your project: "
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "Please include any installation instructions for your project: "
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "Please include any usage information for your project: "
+    },
+    {
+        type: "input",
+        name: "contribution",
+        message: "Please include any contribution guidelines for your project: "
+    },
+    {
+        type: "input",
+        name: "test",
+        message: "Please include any test instructions for your project: "
+    },
+    {
+        type: "checkbox",
+        name: "licenses",
+        message: "Please select any necessary licenses from the list below for your application: ",
+        choices: [
+            "Open Database License",
+            "Mozilla Public License 2.0",
+            "The MIT License",
+            "ISC License"
+        ]
+    },
+    {
+        type: "input",
+        name: "github",
+        message: "Please enter your GitHub username: "
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "Please enter your email address: "
+    }
+];
+
 
 
 // function to write README file
@@ -29,82 +78,17 @@ var inquirer = require("inquirer");
 function init() {
     // Loops through the prompts
     // for (let i = 0; i < questions.length - 1; i++) {
-        inquirer
-            .prompt([
-                
-                // {
-                //     type: types[i],
-                //     name: names[i],
-                //     message: questions[i],
-                //     choices: [
-                //         "Open Database License",
-                //         "Mozilla Public License 2.0",
-                //         "The MIT License",
-                //         "ISC License"
-                //     ]
-                // }
-
-                {
-                    type: "input",
-                    name: "title",
-                    message: "What is the title of your project? "
-                },
-                {
-                    type: "input",
-                    name: "description",
-                    message: "Please write a description for your project: "
-                },
-                {
-                    type: "input",
-                    name: "installation",
-                    message: "Please include any installation instructions for your project: "
-                },
-                {
-                    type: "input",
-                    name: "usage",
-                    message: "Please include any usage information for your project: "
-                },
-                {
-                    type: "input",
-                    name: "contribution",
-                    message: "Please include any contribution guidelines for your project: "
-                },
-                {
-                    type: "input",
-                    name: "test",
-                    message: "Please include any test instructions for your project: "
-                },
-                {
-                    type: "checkbox",
-                    name: "licenses",
-                    message: "Please select any necessary licenses from the list below for your application: ",
-                    choices: [
-                                "Open Database License",
-                                "Mozilla Public License 2.0",
-                                "The MIT License",
-                                "ISC License"
-                    ]
-                },
-                {
-                    type: "input",
-                    name: "github",
-                    message: "Please enter your GitHub username: "
-                },
-                {
-                    type: "input",
-                    name: "email",
-                    message: "Please enter your email address: "
-                },
-            ]).then(function (response) {
-                console.log(response);
-                // Creates the README file
-                // fs.writeFile(`${}`, function (error) {
-                //     if (error) {
-                //         throw error;
-                //     }
-                // });
-            })
-    };
+    inquirer
+        .prompt(questions).then(function (response) {
+            console.log(response);
+            // Creates the README file
+            // fs.writeFile(`${}`, function (error) {
+            //     if (error) {
+            //         throw error;
+            //     }
+            // });
+        })
+};
 // }
 
 // function call to initialize program
