23c9b7567b50f6e2fcf711940dfd3dfe630a4b08
Sean Hopkins <seahopki12@gmail.com>
2020-10-12 15:59:58 -0400

    npm install

--- 02-Homework/Develop/index.js
+++ 02-Homework/Develop/index.js
@@ -5,24 +5,51 @@ var inquirer = require("inquirer");
 const questions = [
     "What is the title of your project? ", "Please write a description for your project: ", "Please include any installation instructions for your project: ", "Please include any usage information for your project: ", "Please include any contribution guidelines for your project: ", "Please include any test instructions for your project: ", "Please select any necessary licenses from the list below for your application: ", "Please enter your GitHub username: ", "Please enter your email address: "
 ];
-
+const names = ["title", "description", "installation", "usage", "contribution", "test", "licenses", "github", "email"];
+const types = ["input", "input", "input", "input", "input", "input", "input", "checkbox", "input", "input"];
 // function to write README file
-function writeToFile(fileName, data) {
+// function writeToFile(fileName, data) {
 
+//     fs.appendFile("README.md", , function (error) {
 
-    // function to initialize program
-    function init() {
-        fs.appendFile("README.md", process.argv[2] + '\n', function (err) {
+//         if (error) {
+//             console.log(error);
+//         }
+//         else {
+//             console.log("Your README file was successfully created!");
+//         }
 
-            if (error) {
-                console.log(error);
-            }
-            else {
-                console.log("Your README file was successfully created!");
-            }
+//     });
+// };
+// function to initialize program
+function init() {
 
-        });
-    }
+    // Loops through the prompts
+    for (var i = 0; i < questions.length - 1; i++) {
+        inquirer
+            .prompt([
+                {
+                    type: types[i],
+                    name: names[i],
+                    message: questions[i],
+                    // choices: [
+                    //     "Open Database License",
+                    //     "Mozilla Public License 2.0",
+                    //     "The MIT License",
+                    //     "ISC License"
+                    // ]
+                }
+            ]).then(function (response) {
+                console.log(response);
+                // Creates the README file
+                // fs.writeFile(`${}`, function (error) {
+                //     if (error) {
+                //         throw error;
+                //     }
+                // });
+            })
+    };
+}
 
-    // function call to initialize program
-    init();
+// function call to initialize program
+init();
