77429c6b5ea22537443210c682572cf840fe9ec1
Sean Hopkins <seahopki12@gmail.com>
2020-10-11 21:13:38 -0400

    included fs and inquirer require statements.

--- 02-Homework/Develop/index.js
+++ 02-Homework/Develop/index.js
@@ -1,15 +1,18 @@
+var fs = require("fs");
+var inquirer = require("inquirer");
+
 // array of questions for user
 const questions = [
-
+    
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
-}
+
 
 // function to initialize program
 function init() {
-
+    
 }
 
 // function call to initialize program
